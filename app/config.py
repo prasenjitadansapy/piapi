89a107fdf06b17470b7a053ad6bdfd9b866b1b8a
Prasenjit Ghosh <prasenjit.ghosh@adansa.com>
2022-03-12 14:57:09 +0530

    saa

--- app/config.py
+++
@@ -1,6 +0,0 @@
-
-class Config:
-    SECRET_KEY = 'ams1234567890ams'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Ams1234@localhost/flasktest'
-
-JWT_SUPER_SECRET_KEY = "46ff09e3-7c97-4045-b127-8ae0276c4085"
